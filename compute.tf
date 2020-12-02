651b725d4678134918f88681b826db5aefd1b1bc
Stefan Oehrli <stefan.oehrli@trivadis.com>
2020-12-02 11:26:37 +0100

    fix tipo

--- compute.tf
+++ compute.tf
@@ -38,15 +38,14 @@ resource "oci_core_instance" "compute" {
 
   metadata = {
     ssh_authorized_keys = var.ssh_public_key != "" ? var.ssh_public_key : file(var.ssh_public_key_path)
-    user_data           = var.host_bootstrap != "" ? var.host_bootstrap : base64encode(templatefile(local.host_bootstrap_template, {
-      yum_upgrade             = var.yum_upgrade
-      authorized_keys         = base64gzip(file(local.ssh_public_key_path))
-      etc_hosts               = base64gzip(file(local.hosts_file))
-      bootstrap_windows_host  = base64gzip(file(local.bootstrap_windows_host))
-      bootstrap_linux_host    = base64gzip(file(local.bootstrap_linux_host))
-    })
+     user_data = var.host_bootstrap != "" ? var.bastion_bootstrap : base64encode(templatefile(local.host_bootstrap_template, {
+      yum_upgrade               = var.yum_upgrade
+      authorized_keys           = base64gzip(file(local.ssh_public_key_path))
+      etc_hosts                 = base64gzip(file(local.hosts_file))
+      bootstrap_windows_host    = base64gzip(file(local.bootstrap_windows_host))
+      bootstrap_linux_host      = base64gzip(file(local.bootstrap_linux_host))
+    })) 
   }
-
   source_details {
     source_type             = "image"
     source_id               = local.host_image_id
